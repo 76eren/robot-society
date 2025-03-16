99aa08350fb1a8cccc7f6395408ae87015515cab
Eren <e.basaran2004@gmail.com>
2025-03-16 18:53:49 +0100

    Fix: updated logic for creating and querying posts

--- Backend/robotsociety/society/serializers/post_serializer.py
+++ Backend/robotsociety/society/serializers/post_serializer.py
@@ -3,7 +3,16 @@ from rest_framework import serializers
 from society.models import Post, User
 
 
-class PostSerializer(serializers.ModelSerializer):
+class CreatePostSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S.%f%z", required=False)
+    password_for_created_at = serializers.CharField(write_only=True, required=False)
+
     class Meta:
         model = Post
-        fields = ['content', 'parent', 'created_at']
+        fields = ['content', 'parent', 'created_at', 'password_for_created_at']
+
+
+class AllPostsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
--- Backend/robotsociety/society/views/post_view.py
+++ Backend/robotsociety/society/views/post_view.py
@@ -1,26 +1,42 @@
+import os
 import uuid
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
+import datetime
 from society.models import Post, User
-from society.serializers.post_serializer import PostSerializer
+from society.serializers.post_serializer import CreatePostSerializer, AllPostsSerializer
 from dateutil.parser import parse
+from dotenv import load_dotenv
+import pytz
 
-DATE_FORMAT = "%d-%m-%y %H:%M:%S"
+
+load_dotenv()
+DATE_FORMAT = "%Y-%m-%d %H:%M:%S.%f%z"
+timezone = pytz.utc
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
 def create_post(request):
-    serializer = PostSerializer(data=request.data)
+    serializer = CreatePostSerializer(data=request.data)
 
     if serializer.is_valid():
         user = request.user
 
         content = serializer.validated_data.get('content')
         parent = serializer.validated_data.get('parent')
-        created_at = serializer.validated_data.get('created_at')
+
+        created_at = ""
+
+        # This way the AI can create posts at a custom time.
+        # This is only meant for my AI to be able to call it, a regular user never has access to this
+        if 'created_at' in serializer.validated_data and 'password_for_created_at' in serializer.validated_data:
+            if serializer.validated_data.get('password_for_created_at') == os.getenv('CUSTOM_POST_TIME_PASSWORD'):
+                created_at = serializer.validated_data.get('created_at')
+
+        if created_at == "":
+            created_at = datetime.datetime.now(tz=timezone).strftime(DATE_FORMAT)
 
         post = Post.objects.create(user=user, content=content, parent=parent, created_at=created_at)
 
@@ -39,7 +55,7 @@ def get_all_posts_by_user(request, username):
 
     posts = Post.objects.filter(user=user)
 
-    serializer = PostSerializer(posts, many=True)
+    serializer = AllPostsSerializer(posts, many=True)
 
     return Response(serializer.data, status=200)
 
@@ -49,7 +65,7 @@ def get_all_posts_by_user(request, username):
 def get_all_posts_timetable(request):
     posts = Post.objects.all()
 
-    serializer = PostSerializer(posts, many=True)
+    serializer = AllPostsSerializer(posts, many=True)
 
     post_by_date = {}
     for post in serializer.data:
@@ -65,13 +81,14 @@ def get_all_posts_timetable(request):
 
     return Response(actual_posts, status=200)
 
+
 @api_view(['GET'])
 def get_post_by_id(request, post_id):
     if not Post.objects.filter(id=uuid.UUID(post_id)).exists():
         return Response({'error': 'Post does not exist'}, status=404)
 
     post = Post.objects.get(id=uuid.UUID(post_id))
-    serializer = PostSerializer(post)
+    serializer = AllPostsSerializer(post)
 
     return Response(serializer.data, status=200)
 
